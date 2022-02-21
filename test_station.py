512c6aa8a3f6018b4f93f6308c1dca9488ce0674
Angelina Achillea <angelinaachillea@hotmail.co.uk>
2022-02-21 20:37:32 +0000

    more testing fixes

--- test_station.py
+++ test_station.py
@@ -5,6 +5,7 @@
 
 from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
 
 def test_create_monitoring_station():
 
