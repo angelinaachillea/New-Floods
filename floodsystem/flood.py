26de99cbb0198b81cf62b0ab5ff19f923a918475
Umair-ahmad1 <ua225@cam.ac.uk>
2022-02-24 18:17:46 +0000

    2c work

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -5,9 +5,11 @@ from .utils import sorted_by_key, gt_with_none
 def stations_level_over_threshold(stations, tol):
     for station in stations:
         tol= tol
-        if station.relative_water_level() > tol
+        if station.relative_water_level() > tol:
     return sorted_by_key(stations, station.relative_water_level())
     
 def stations_highest_rel_level(stations, N):
-    for i in stations_level_over_threshold(stations, tol):
-        return [i[0] 
+    for station in stations:
+        x= sorted_by_key(stations,station.latest_level,True)
+        At_risk_stations=x[:N]
+    return At_risk_stations
