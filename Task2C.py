35a1e544e15bcd450a7a011bc0408e4bacd6f78c
Umair-ahmad1 <ua225@cam.ac.uk>
2022-02-24 18:17:51 +0000

    2c work

--- Task2C.py
+++ Task2C.py
@@ -1,1 +1,12 @@
 #test to seee if working
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+
+stations=build_station_list
+update_water_levels(stations)
+N=10
+for station_tuple in stations_highest_rel_level(stations,N):
+    print(station_tuple[0].name + " "+str(station_tuple[1]))
+    
+
