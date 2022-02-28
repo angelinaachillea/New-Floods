c5b2e0b5342a18011b343c3e75a01cc5d436d490
Angelina Achillea <angelinaachillea@hotmail.co.uk>
2022-02-28 13:01:58 +0000

    another attempt at c

--- Task2C.py
+++ Task2C.py
@@ -2,11 +2,14 @@
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
 
-
-stations=build_station_list
-update_water_levels(stations)
-N=10
-for station_tuple in stations_highest_rel_level(stations,N):
-    print(station_tuple[0].name + " "+str(station_tuple[1]))
+def run():
+    stations=build_station_list()
+    update_water_levels(stations)
+    #N=10
+    for station in stations_highest_rel_level(stations,10):
+        print(station.name + " " + str(station.relative_water_level()))
     
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+    run()
 
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -7,11 +7,17 @@ def stations_level_over_threshold(stations, tol):
        # if gt_with_none(station.relative_water_level(), tol) and station.name != "Letcombe Bassett"], 1, True)
     #return sorted_by_key(stations, station.relative_water_level())
     return sorted_by_key([(station, station.relative_water_level()) for station in stations if gt_with_none(station.relative_water_level(), tol) and station.name != "Letcombe Bassett"], 1, True)
-    
+
+'''
 def stations_highest_rel_level(stations, N):
     for station in stations:
         x= sorted_by_key(stations,station.latest_level,True)
         At_risk_stations=x[:N]
     return At_risk_stations
+'''
+def stations_highest_rel_level(stations, N):
+    x[0]
+    for x in stations_level_over_threshold(stations,-9999.9):
+        return x[0:N]
 
 
