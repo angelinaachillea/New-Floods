c9ed03d12b7b7f37e2013eb17fb80b1f6530007c
Angelina Achillea <angelinaachillea@hotmail.co.uk>
2022-02-28 12:30:44 +0000

    task 2B complete

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,15 +1,17 @@
 from secrets import token_urlsafe
-from .utils import sorted_by_key, gt_with_none
+from .utils import sorted_by_key, gt_with_none 
 
 
 def stations_level_over_threshold(stations, tol):
-    for station in stations:
-        tol= tol
-        if station.relative_water_level() > tol:
-    return sorted_by_key(stations, station.relative_water_level())
+    #for station in stations:
+       # if gt_with_none(station.relative_water_level(), tol) and station.name != "Letcombe Bassett"], 1, True)
+    #return sorted_by_key(stations, station.relative_water_level())
+    return sorted_by_key([(station, station.relative_water_level()) for station in stations if gt_with_none(station.relative_water_level(), tol) and station.name != "Letcombe Bassett"], 1, True)
     
 def stations_highest_rel_level(stations, N):
     for station in stations:
         x= sorted_by_key(stations,station.latest_level,True)
         At_risk_stations=x[:N]
     return At_risk_stations
+
+
