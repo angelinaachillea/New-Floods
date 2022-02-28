a5c331545dc2c2bf955d114a5efa7f1a34b5427f
Umair-ahmad1 <ua225@cam.ac.uk>
2022-02-28 20:11:43 +0000

    task 2e complete

--- Task2E.py
+++ Task2E.py
@@ -2,7 +2,7 @@ from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.plot import plot_water_levels
-from datetime import datetime, timedelta
+import datetime
 
 
 def run():
