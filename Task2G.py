78d7571df9d9eb25a61c548e56697e1c3a8040f2
Umair-ahmad1 <ua225@cam.ac.uk>
2022-03-01 13:30:52 +0000

    test for flood functions

--- Task2G.py
+++ Task2G.py
@@ -1,8 +1,3 @@
-#build a set of data of the stations risk of flooding 
-#whichever are most at risk of flooding then use that list and relate to the towns in which they flow through
-#to get the rate of flooding use the fraction of over usual flow rate as given in exercise 2B
-#that will give the risk of flooding if you take anything at 0.8 and below as low risk,0.8-1.2 medium risk,1.2-2 high risk, 2+severe
-#with all these scores for the stations (0,1,2,3) add them up for all the rivers in the area, 
 
 
 from floodsystem.datafetcher import fetch_measure_levels
@@ -112,6 +107,7 @@ def run():
     for town in moderate_risk_towns:    
         if town is not None:
             print(town)
+            
 if __name__=="__main__":
     print("***Task 2G: CUED IA Flood Warning Project***")
     run()
---
+++ test_flood.py
@@ -0,0 +1,27 @@
+
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
+def test_stations_level_over_threshold():
+    
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    stations_above_threshold = stations_level_over_threshold(stations, 0.9)
+    for i in range(len(stations_above_threshold) - 1):
+        assert stations_above_threshold[i][1] >= stations_above_threshold[i + 1][1]
+        assert stations_above_threshold[i][1] >= 0.9
+
+
+def test_stations_highest_rel_level():
+
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    highest_flowers = stations_highest_rel_level(stations, 10)
+    assert len(highest_flowers) == 10
+    #test values are sorted in decreasing order
+    for i in range(len(highest_flowers) - 1):
+        assert highest_flowers[i].relative_water_level() > highest_flowers[i + 1].relative_water_level()
