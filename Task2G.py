983ebda3771036c958c61e69e3562e6052b80b2e
Umair-ahmad1 <ua225@cam.ac.uk>
2022-03-01 10:09:35 +0000

    2g work

--- Task2G.py
+++ Task2G.py
@@ -3,3 +3,97 @@
 #to get the rate of flooding use the fraction of over usual flow rate as given in exercise 2B
 #that will give the risk of flooding if you take anything at 0.8 and below as low risk,0.8-1.2 medium risk,1.2-2 high risk, 2+severe
 #with all these scores for the stations (0,1,2,3) add them up for all the rivers in the area, 
+
+
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_level_over_threshold
+from floodsystem.analysis import polyfit
+import datetime
+import matplotlib
+import numpy as np
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+    dt = 10
+
+    stations = stations_level_over_threshold(stations, -999)
+    stations_risk_level=[]
+    for station_tuple in stations:
+        if station_tuple[1] <= 1.2:
+            stations_risk_level.append(station_tuple)
+            continue
+        station = station_tuple[0]
+        skip = False
+        try:
+            print("Fetching levels for " + station.name)
+            results = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        except KeyError:
+            print("Levels for " + station.name + " not found.")
+            skip = True
+        if skip:
+            continue
+        try:
+            x=matplotlib.dates.date2num(results[0])
+            poly, _ = polyfit(results[0],results[1],4)
+        except TypeError:
+            print("Cannot process retrieved data.")
+            skip = True
+        if skip:
+            continue
+
+        severity = 0
+
+        levels_tomorrow = poly(1)
+        levels_in_two_days = poly(2)
+        rel_level_tomorrow = levels_tomorrow - station.typical_range[0]
+        rel_level_tomorrow /= station.typical_range[1] - station.typical_range[0]
+        rel_level_in_two_days = levels_in_two_days - station.typical_range[0]
+        rel_level_in_two_days /= station.typical_range[1] - station.typical_range[0]
+
+        severity += station_tuple[1]
+        if rel_level_tomorrow > 1.5:
+            severity += station_tuple[1]
+        if rel_level_in_two_days > 2:
+            severity += station_tuple[1]
+        stations_risk_level.append((station, severity))
+
+    low_risk = []
+    moderate_risk = []
+    high_risk = []
+    severe_risk = []
+
+    for station_tuple in stations_risk_level:
+        if station_tuple[1] < 1:
+            low_risk.append(station_tuple[0])
+        elif station_tuple[1] < 4:
+            moderate_risk.append(station_tuple[0])
+        elif station_tuple[1] < 6:
+            high_risk.append(station_tuple[0])
+        else:
+            severe_risk.append(station_tuple[0])
+    # now with list of stations 
+    severe_risk_towns=[]
+    high_risk_towns=[]
+    moderate_risk_towns=[]
+    low_risk_towns=[]
+    for station_tuple in stations_risk_level:
+        if station in severe_risk:
+            severe_risk_towns.append(station.town)
+        elif station in high_risk:
+            high_risk_towns.append(station.town)
+        elif station in moderate_risk:
+            moderate_risk_towns.append(station.town)
+        else:
+            low_risk_towns.append(station.town)
+    print("towns at severe risk")
+    print(severe_risk_towns)
+    print("towns at high risk")
+    print(high_risk_towns)
+    print("towns at moderate risk")
+    print(moderate_risk_towns)
+    print("towns at low risk")
+    print(low_risk_towns)
+if __name__=="main":
+    print("***Task 2G: CUED IA Flood Warning Project***")
+    run()
