6c5b5166223458ebf041bbe691a4061248561239
Umair-ahmad1 <ua225@cam.ac.uk>
2022-03-01 14:03:57 +0000

    tests finished

--- test_analysis.py
+++ test_analysis.py
@@ -10,8 +10,8 @@ def test_polyfit():
     stations= build_station_list()
     update_water_levels(stations)
     station= stations[0] # pick a station randomly
-    results= fetch_measure_levels(station.measure_id,dt=datetime.timedelta(days=2))
-    poly, d0 = polyfit(results[0],results[1],4)
-    assert type(poly) == type(np.poly1d([1]))
+    results= fetch_measure_levels(station.measure_id,dt=datetime.timedelta(days=2)) #take data from 2 days back
+    poly, d0 = polyfit(results[0],results[1],4) #results[0]=dates, results[1]= levels, 4 is the degree for the least squares fit
+    assert type(poly) == type(np.poly1d([1])) #the function from the analysis section has its own type
     assert d0 == matplotlib.dates.date2num(results[0])[0]
 
